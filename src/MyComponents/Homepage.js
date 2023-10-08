4b13da5c67657c97f6750b82d6e6c7f0b76d1569
PRASHIKGITHUB <prashik.bahadure5@gamil.com>
2023-10-08 21:24:29 +0530

    latest

--- src/MyComponents/Homepage.js
+++ src/MyComponents/Homepage.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import './Homepage.css'
-// import Navbar from './Navbar'
+import myImage from './dog1.avif';
+
 const Homepage = () => {
   return (
     <div id='x'>
       <div id="innerx">
 
-        <div id="rightAni">
-          {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ190PfuCnwOEMMAaNaWOBzahmJcu1s8hs53MIikI8ETtU9mUFGvKL7I7S5hGVFa1Mxqo&usqp=CAU" alt="" /> */}
-        </div>
+        
           
         <div id="img">
           <div id="Webname">
             <h1>Pet Adoption</h1>
           </div>
           
-          <img src="https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg?w=1380&t=st=1696570494~exp=1696571094~hmac=c598fea2d449bf653d2040ddd04fe8657d6b9bdac9336ec4cd99320a3d741ed6" alt="" />
+          <img src={myImage} alt="" />
+          
         </div>
 
-        <div id="leftAni"></div>
+        
 
       </div>
 
--- src/MyComponents/Testimonials.js
+++ src/MyComponents/Testimonials.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './Testimonials.css';
+import Image from './img.jpg';
 
 const Testimonials = () => {
   return (
     <div id='testi'>
         <div id="innerT">
-            <div id="card">Prashik</div>
+            <div id="card">
+              <img src={Image} alt="" />
+              <div id="PetDis">
+                <h1>Name : Pairs</h1>
+                <br />
+                <h2>Age : Puppy</h2>
+                <br />
+                <h2>Color : Brown</h2>
+                <br />
+                <h2>Vaccinated : No</h2>
+                <br />
+                <h2>Breed : Aspin</h2>
+                <br />
+                <h2>Information : Playful</h2>
+                <button>
+                  View Post
+                </button>
+
+
+              </div>
+            </div>
             <div id="card">Himduyti</div>
             <div id="card">Ajay</div>
         </div>
